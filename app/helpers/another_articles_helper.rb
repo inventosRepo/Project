3c161a06c9610a7f8bd314bb970e88098b3066b5
Sergey <roverpadinside@gmail.com>
2016-05-10 16:19:57 +0300

    Deleted useless files

--- app/helpers/another_articles_helper.rb
+++
@@ -1,2 +0,0 @@
-module AnotherArticlesHelper
-end
--- db/migrate/20160508153719_create_passwords.rb
+++
@@ -1,8 +0,0 @@
-class CreatePasswords < ActiveRecord::Migration
-  def change
-    create_table :passwords do |t|
-      t.string :email,              null: false, default: ""
-      t.string :password,           null: false, default: ""
-    end
-  end
-end

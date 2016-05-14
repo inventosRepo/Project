724738833d9348ef17a151b8977d44e76041d430
bigbromen <89536188623@mail.ru>
2016-05-14 18:48:38 +0300

    move js and css code from view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,26 +1,27 @@
 class WelcomeController < ApplicationController
   require 'em-websocket'
-  def index
-    @ip = APP_CONFIG[:ws_ip]
-    @port = APP_CONFIG[:ws_port]
-    @clients = []
-    EM::WebSocket.start(host: @ip, port: @port) do |ws|
-      ws.onopen do |_handshake|
-        @clients << ws
-        ws.send 'Connected'
-      end
+  @ip = APP_CONFIG[:ws_ip]
+  @port = APP_CONFIG[:ws_port]
+  @clients = []
+  EM::WebSocket.start(host: @ip, port: @port) do |ws|
+    ws.onopen do |_handshake|
+      @clients << ws
+      ws.send 'Connected'
+    end
 
-      ws.onclose do
-        ws.send 'Closed.'
-        @clients.delete ws
-      end
+    ws.onclose do
+      ws.send 'Closed.'
+      @clients.delete ws
+    end
 
-      ws.onmessage do |msg|
-        puts "Received Message: #{msg}"
-        @clients.each do |socket|
-          socket.send msg
-        end
+    ws.onmessage do |msg|
+      puts "Received Message: #{msg}"
+      @clients.each do |socket|
+        socket.send msg
       end
     end
+  end
+
+  def index
   end
 end

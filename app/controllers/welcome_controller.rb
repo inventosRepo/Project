6e5da82ce163526a4bd61e8e7e6af7afb17f2e0c
bigbromen <89536188623@mail.ru>
2016-05-19 11:02:03 +0300

    Touching fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,12 +9,13 @@ class WelcomeController < ApplicationController
   end
 
   def new
-    if !Field.exists?(player1: current_user.email)
+    if !Field.exists?(player1: current_user.email) && !Field.exists?(player2: current_user.email)
       field = Field.new
       Field.crtgfield(field, current_user.email)
-        @user = User.where(email: current_user.email).take
-        @user.playersid = 1
-        @user.save
+
+      @user = User.where(email: current_user.email).take
+      @user.playersid = 1
+      @user.save
     end
     redirect_to user_root_path
   end
@@ -23,13 +24,14 @@ class WelcomeController < ApplicationController
     if !Field.exists?(player1: current_user.email)
       @currentmail = params[:currentgame]
       @field = Field.where(player1: @currentmail).take
-      @user = User.where(email: current_user.email).take
       @field.player2 = current_user.email
-      @field.count = 2
-      @user.playersid = 2
+      @field.count = 2      
       @field.save
+
+      @user = User.where(email: current_user.email).take
+      @user.playersid = 2
       @user.save
-      redirect_to user_root_path
     end
+    redirect_to user_root_path
   end
 end

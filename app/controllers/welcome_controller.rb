991c72d7a30793d9e476767c4cce3790a5ad36d3
bigbromen <89536188623@mail.ru>
2016-06-07 18:04:31 +0300

    Fixed load map when game dint create

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,27 +10,29 @@ class WelcomeController < ApplicationController
                       else
                         Field.where(player2: current_user.email).take
                       end
+      level = User.where(email: current_user.email).take
+      map = Level.find(level.user_level)
       unless @current_game.nil?
         if user_signed_in? && @current_game.save_game == 0 && !@current_game.nil?
-          level = User.where(email: current_user.email).take
-          map = Level.find(level.user_level)
           @level_map = map.data
           @arr_tanks = @current_game.save_arrs_tanks
         else
           @level_map = @current_game.map
-          @arr_tanks = @current_game.save_arrs_tank
+          @arr_tanks = @current_game.save_arrs_tanks
         end
         gon.arr_tanks = @arr_tanks
 
         @save_level = params[:lvlmap]
         @save_arrs_tanks = params[:arrs_tanks]
-
+        # Запись в бд
         unless @save_level.nil?
           @current_game.map = @save_level
           @current_game.save_arrs_tanks = @save_arrs_tanks
           @current_game.save_game = 1
           @current_game.save
         end
+      else
+        @level_map = map.data
       end
     end
 

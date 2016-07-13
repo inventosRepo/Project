4f972ca9502dae5629f6222f603867eb0b7f238f
bigbromen <89536188623@mail.ru>
2016-07-13 12:39:00 +0300

    added a connection to field's only if the user has created this game , or connected to it(test commit)

--- app/assets/javascripts/multi_player_logic.js
+++ app/assets/javascripts/multi_player_logic.js
@@ -4,195 +4,213 @@ function connect() {
     var button = msg.button_id;
     var action = msg.action;
     var player = msg.player_id;
-
-    if (action == 'connect') {
-      console.log(msg);
-      player_live[player] = true;
-      tank[player] = new Tank;
-      tank[player].set_pos(player);
-    }
-    if (player_live[player] == true) {
-      if (button == 1) {
-        for (x = 1; x < tank.length; x++) {
-          if (tank[x]!=null) {
-            tank_cell_x[x] = parseInt(tank[x].x/cell_size);
-            tank_cell_y[x] = parseInt(tank[x].y/cell_size);
-          }
-        }
-        tank[player].i = 1;
-        var cur_cel_x = (2 * tank[player].x) / 48;
-        var cur_cel_y = (2 * tank[player].y) / 48;
-        var test_1 = level_map[cur_cel_y][cur_cel_x-1];
-        var test_2 = level_map[cur_cel_y+1][cur_cel_x-1];
-        if (tank.length > 2) {
-          for (x = 1; x < tank.length; x++) {
-            if ( ( (tank_cell_x[player] != (tank_cell_x[x]+2)) &&
-                   (tank_cell_y[player] == (tank_cell_y[x])) &&
-                   (x!=player) ) || (tank_cell_y[player] != (tank_cell_y[x]) ) ) {
-                     if ((test_1 == 0 || test_1 == 3) && (test_2 == 0 || test_2 == 3)) {
-                       tank[player].x-=cell_size;
-                       if (tank[player].x < 0) {
-                         tank[player].x = 0;
-                       }
-                     }
-                     continue;
+    var id = msg.tank_id;
+    if(host_player_f_id == msg.player_id_field){
+      if (action == 'connect') {
+        console.log(msg);
+        player_live[player] = true;
+        tank[player] = new Tank;
+        set_pos(player);
+        function set_pos(player) {
+          $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: "/multiplayer/set_pos",
+            data: { user_id: id },
+            success: function (data) {
+              console.log(data);
+              tank[player].x = data.player_x;
+              tank[player].y = data.player_y;
+              tank[player].i = data.player_i;
             }
-          }
+          });
         }
-        else {
-          if ((test_1 == 0 || test_1 == 3) && (test_2 == 0 || test_2 == 3)) {
-            tank[player].x-=cell_size;
-            if (tank[player].x < 0) {
-              tank[player].x = 0;
-            }
-          }
-        };
       }
 
-      if (button == 2) {
-        for (x = 1; x < tank.length; x++) {
-          if (tank[x]!=null) {
-            tank_cell_x[x] = parseInt(tank[x].x/cell_size)
-            tank_cell_y[x] = parseInt(tank[x].y/cell_size);
+      if (player_live[player] == true) {
+        if (button == 1) {
+          for (x = 1; x < tank.length; x++) {
+            if (tank[x]!=null) {
+              tank_cell_x[x] = parseInt(tank[x].x/cell_size);
+              tank_cell_y[x] = parseInt(tank[x].y/cell_size);
+            }
           }
+          tank[player].i = 1;
+          var cur_cel_x = (2 * tank[player].x) / 48;
+          var cur_cel_y = (2 * tank[player].y) / 48;
+          var test_1 = level_map[cur_cel_y][cur_cel_x-1];
+          var test_2 = level_map[cur_cel_y+1][cur_cel_x-1];
+          if (tank.length > 2) {
+            for (x = 1; x < tank.length; x++) {
+              if ( ( (tank_cell_x[player] != (tank_cell_x[x]+2)) &&
+                     (tank_cell_y[player] == (tank_cell_y[x])) &&
+                     (x!=player) ) || (tank_cell_y[player] != (tank_cell_y[x]) ) ) {
+                       if ((test_1 == 0 || test_1 == 3) && (test_2 == 0 || test_2 == 3)) {
+                         tank[player].x-=cell_size;
+                         if (tank[player].x < 0) {
+                           tank[player].x = 0;
+                         }
+                       }
+                       continue;
+              }
+            }
+          }
+          else {
+            if ((test_1 == 0 || test_1 == 3) && (test_2 == 0 || test_2 == 3)) {
+              tank[player].x-=cell_size;
+              if (tank[player].x < 0) {
+                tank[player].x = 0;
+              }
+            }
+          };
         }
 
-        tank[player].i = 2;
-
-        var cur_cel_x = (2 * tank[player].x) / 48;
-        var cur_cel_y = (2 * tank[player].y) / 48;
-        var test_1 = level_map[cur_cel_y-1][cur_cel_x];
-        var test_2 = level_map[cur_cel_y-1][cur_cel_x+1];
-
-        if (tank.length > 2) {
+        if (button == 2) {
           for (x = 1; x < tank.length; x++) {
-            if ( ( (tank_cell_y[player] != (tank_cell_y[x]+2)) &&
-                   (tank_cell_x[player] == (tank_cell_x[x])) &&
-                   (x!=player) ) || (tank_cell_x[player] != (tank_cell_x[x]) ) ) {
-                  if ((test_1 == 0 || test_1 == 3) && (test_2 == 0 || test_2 == 3)) {
-                    tank[player].y-=cell_size;
-                    if (tank[player].y < 0) {
-                      tank[player].y = 0;
+            if (tank[x]!=null) {
+              tank_cell_x[x] = parseInt(tank[x].x/cell_size)
+              tank_cell_y[x] = parseInt(tank[x].y/cell_size);
+            }
+          }
+
+          tank[player].i = 2;
+
+          var cur_cel_x = (2 * tank[player].x) / 48;
+          var cur_cel_y = (2 * tank[player].y) / 48;
+          var test_1 = level_map[cur_cel_y-1][cur_cel_x];
+          var test_2 = level_map[cur_cel_y-1][cur_cel_x+1];
+
+          if (tank.length > 2) {
+            for (x = 1; x < tank.length; x++) {
+              if ( ( (tank_cell_y[player] != (tank_cell_y[x]+2)) &&
+                     (tank_cell_x[player] == (tank_cell_x[x])) &&
+                     (x!=player) ) || (tank_cell_x[player] != (tank_cell_x[x]) ) ) {
+                    if ((test_1 == 0 || test_1 == 3) && (test_2 == 0 || test_2 == 3)) {
+                      tank[player].y-=cell_size;
+                      if (tank[player].y < 0) {
+                        tank[player].y = 0;
+                      }
                     }
-                  }
-                  continue;
+                    continue;
+              }
             }
           }
-        }
-        else {
-          if ((test_1 == 0 || test_1 == 3) && (test_2 == 0 || test_2 == 3)) {
-            tank[player].y-=cell_size;
-            if (tank[player].y < 0) {
-              tank[player].y = 0;
+          else {
+            if ((test_1 == 0 || test_1 == 3) && (test_2 == 0 || test_2 == 3)) {
+              tank[player].y-=cell_size;
+              if (tank[player].y < 0) {
+                tank[player].y = 0;
+              }
             }
-          }
-        };
-      }
-
-      if (button == 3) {
-        for (x = 1; x < tank.length; x++) {
-          if (tank[x]!=null) {
-            tank_cell_x[x] = parseInt(tank[x].x/cell_size)
-            tank_cell_y[x] = parseInt(tank[x].y/cell_size);
-          }
+          };
         }
 
-        tank[player].i = 3;
-
-        var cur_cel_x = (2 * tank[player].x) / 48;
-        var cur_cel_y = (2 * tank[player].y) / 48;
-        var test_1 = level_map[cur_cel_y+2][cur_cel_x];
-        var test_2 = level_map[cur_cel_y+2][cur_cel_x+1];
-
-        if (tank.length > 2) {
+        if (button == 3) {
           for (x = 1; x < tank.length; x++) {
-            if ( ( (tank_cell_y[player] != (tank_cell_y[x]-2)) &&
-                   (tank_cell_x[player] == (tank_cell_x[x])) &&
-                   (x!=player) ) || (tank_cell_x[player] != (tank_cell_x[x]) ) ) {
-                     if ((test_1 == 0 || test_1 == 3) && (test_2 == 0 || test_2 == 3)) {
-                       tank[player].y+=cell_size;
-                       if (tank[player].y > cell_size * y_count) {
-                         tank[player].y = cell_size * y_count;
+            if (tank[x]!=null) {
+              tank_cell_x[x] = parseInt(tank[x].x/cell_size)
+              tank_cell_y[x] = parseInt(tank[x].y/cell_size);
+            }
+          }
+
+          tank[player].i = 3;
+
+          var cur_cel_x = (2 * tank[player].x) / 48;
+          var cur_cel_y = (2 * tank[player].y) / 48;
+          var test_1 = level_map[cur_cel_y+2][cur_cel_x];
+          var test_2 = level_map[cur_cel_y+2][cur_cel_x+1];
+
+          if (tank.length > 2) {
+            for (x = 1; x < tank.length; x++) {
+              if ( ( (tank_cell_y[player] != (tank_cell_y[x]-2)) &&
+                     (tank_cell_x[player] == (tank_cell_x[x])) &&
+                     (x!=player) ) || (tank_cell_x[player] != (tank_cell_x[x]) ) ) {
+                       if ((test_1 == 0 || test_1 == 3) && (test_2 == 0 || test_2 == 3)) {
+                         tank[player].y+=cell_size;
+                         if (tank[player].y > cell_size * y_count) {
+                           tank[player].y = cell_size * y_count;
+                         }
                        }
-                     }
-                     continue;
+                       continue;
+              }
             }
           }
-        }
-        else {
-          if ((test_1 == 0 || test_1 == 3) && (test_2 == 0 || test_2 == 3)) {
-            tank[player].y+=cell_size;
-            if (tank[player].y > cell_size * y_count) {
-              tank[player].y = cell_size * y_count;
+          else {
+            if ((test_1 == 0 || test_1 == 3) && (test_2 == 0 || test_2 == 3)) {
+              tank[player].y+=cell_size;
+              if (tank[player].y > cell_size * y_count) {
+                tank[player].y = cell_size * y_count;
+              }
             }
-          }
-        };
-      }
-
-      if (button == 4) {
-        for (x = 1; x < tank.length; x++) {
-          if (tank[x]!=null) {
-            tank_cell_x[x] = parseInt(tank[x].x/cell_size);
-            tank_cell_y[x] = parseInt(tank[x].y/cell_size);
-          }
+          };
         }
 
-        tank[player].i = 0;
-
-        var cur_cel_x = (2 * tank[player].x) / 48;
-        var cur_cel_y = (2 * tank[player].y) / 48;
-        var test_1 = level_map[cur_cel_y][cur_cel_x+2];
-        var test_2 = level_map[cur_cel_y+1][cur_cel_x+2];
-
-        if (tank.length > 2) {
+        if (button == 4) {
           for (x = 1; x < tank.length; x++) {
-            if ( ( (tank_cell_x[player] != (tank_cell_x[x]-2)) &&
-                   (tank_cell_y[player] == (tank_cell_y[x])) &&
-                   (x!=player) ) || (tank_cell_y[player] != (tank_cell_y[x]) ) ) {
-                     if ((test_1 == 0 || test_1 == 3) && (test_2 == 0 || test_2 == 3)) {
-                       tank[player].x+=cell_size;
-                       if (tank[player].x > cell_size * x_count) {
-                         tank[player].x = cell_size * x_count;
+            if (tank[x]!=null) {
+              tank_cell_x[x] = parseInt(tank[x].x/cell_size);
+              tank_cell_y[x] = parseInt(tank[x].y/cell_size);
+            }
+          }
+
+          tank[player].i = 0;
+
+          var cur_cel_x = (2 * tank[player].x) / 48;
+          var cur_cel_y = (2 * tank[player].y) / 48;
+          var test_1 = level_map[cur_cel_y][cur_cel_x+2];
+          var test_2 = level_map[cur_cel_y+1][cur_cel_x+2];
+
+          if (tank.length > 2) {
+            for (x = 1; x < tank.length; x++) {
+              if ( ( (tank_cell_x[player] != (tank_cell_x[x]-2)) &&
+                     (tank_cell_y[player] == (tank_cell_y[x])) &&
+                     (x!=player) ) || (tank_cell_y[player] != (tank_cell_y[x]) ) ) {
+                       if ((test_1 == 0 || test_1 == 3) && (test_2 == 0 || test_2 == 3)) {
+                         tank[player].x+=cell_size;
+                         if (tank[player].x > cell_size * x_count) {
+                           tank[player].x = cell_size * x_count;
+                         }
                        }
-                     }
-                     continue;
+                       continue;
+              }
             }
           }
+          else {
+            if ((test_1 == 0 || test_1 == 3) && (test_2 == 0 || test_2 == 3)) {
+              tank[player].x+=cell_size;
+              if (tank[player].x > cell_size * x_count) {
+                tank[player].x = cell_size * x_count;
+              }
+            }
+          };
         }
-        else {
-          if ((test_1 == 0 || test_1 == 3) && (test_2 == 0 || test_2 == 3)) {
-            tank[player].x+=cell_size;
-            if (tank[player].x > cell_size * x_count) {
-              tank[player].x = cell_size * x_count;
-            }
-          }
-        };
-      }
 
-      if (button == 5) {
-        bullets[player].push(new Bullet(tank[player].i, tank[player].x+12, tank[player].y+12, 1));
-      }
+        if (button == 5) {
+          bullets[player].push(new Bullet(tank[player].i, tank[player].x+12, tank[player].y+12, 1));
+        }
 
-      if (button == 6) {
-        $.ajax({
-          url: "/multiplayer/set_pos",
-          type: "POST",
-          dataType: "json",
-          data: { user_id: player,
-                  pos_x: JSON.stringify(tank[player].x),
-                  pos_y: JSON.stringify(tank[player].y),
-                  pos_i: JSON.stringify(tank[player].i)}
-        });
+        if (button == 6) {
+          $.ajax({
+            url: "/multiplayer/set_pos",
+            type: "POST",
+            dataType: "json",
+            data: { user_id: id,
+                    pos_x: JSON.stringify(tank[player].x),
+                    pos_y: JSON.stringify(tank[player].y),
+                    pos_i: JSON.stringify(tank[player].i)}
+          });
+        }
       }
     }
   }
-}
 
-function post_level_map(){
-  $.ajax({
-    url: "/multiplayer/index",
-    type: "POST",
-    dataType: "json",
-    data: { lvlmap: JSON.stringify(level_map) }
-  });
-}
+  function post_level_map(){
+    $.ajax({
+      url: "/multiplayer/index",
+      type: "POST",
+      dataType: "json",
+      data: { lvlmap: JSON.stringify(level_map) }
+    });
+  }
+
+    }
--- public/scripts/direction.js
+++ public/scripts/direction.js
@@ -1,71 +1,75 @@
   window.onload = function() {
-    var port = 3010;
-    var host = "ws://"+window.location.hostname.toString()+":"+port;
-    var socket = [];
-    var btn_id;
-    var bonus = 1;
-    var move_speed = 300*bonus;
-    var fire_speed = 700*bonus;
+          var port = 3010;
+          var host = "ws://"+window.location.hostname.toString()+":"+port;
+          var socket = [];
+          var btn_id;
+          var bonus = 1;
+          var move_speed = 300*bonus;
+          var fire_speed = 700*bonus;
 
-    function connect(player) {
-      try {
-        socket[player] = new WebSocket(host);
+          function connect(player) {
+            try {
+              socket[player] = new WebSocket(host);
 
-        socket[player].onopen = function () {
-          var msg = {
-            player_id: player,
-            button_id: 0,
-            action: 'connect'
-          };
-          socket[player].send(JSON.stringify(msg));
+              socket[player].onopen = function () {
+                var msg = {
+                  player_id: player,
+                  tank_id: id_tank,
+                  button_id: 0,
+                  player_id_field: player_id_field,
+                  action: 'connect'
+                };
+                socket[player].send(JSON.stringify(msg));
+              }
+            } catch(exception) {
+              addMessage("Error: " + exception);
+            }
+          }
+
+          function send(player) {
+            var msg = {
+              player_id: player,
+              button_id: btn_id,
+              tank_id: id_tank,
+              player_id_field: player_id_field,
+              action: 'send'
+            };
+            try {
+              socket[player].send(JSON.stringify(msg));
+            } catch (exception) {
+              addMessage("Failed To Send")
+            }
+          }
+
+
+
+          function btn_event(id, bt_id, speed) {
+            document.getElementById(id).ontouchstart =  function() {
+            var my_interval = setInterval(function() { btn_id = bt_id; send(player); }, speed);
+            this.ontouchend = function(){ clearInterval(my_interval); }
+            }
+          }
+
+          function save_event(id, bt_id) {
+            document.getElementById(id).onclick =  function() {
+              btn_id = bt_id; send(player);
+            }
+          }
+
+          function _port(id, prt) {
+            document.getElementById(id).onclick =  function() {
+              port = prt;
+              host = "ws://"+window.location.hostname.toString()+":"+port;
+              connect(player);
+            }
+          }
+
+          connect(player);
+
+          btn_event("left", 1, move_speed);
+          btn_event("top", 2, move_speed);
+          btn_event("down", 3, move_speed);
+          btn_event("right", 4, move_speed);
+          btn_event("fire", 5, fire_speed);
+          save_event("save", 6);
         }
-      } catch(exception) {
-        addMessage("Error: " + exception);
-      }
-    }
-
-    function send(player) {
-      var msg = {
-        player_id: player,
-        button_id: btn_id,
-        action: 'send'
-      };
-      try {
-        socket[player].send(JSON.stringify(msg));
-      } catch (exception) {
-        addMessage("Failed To Send")
-      }
-    }
-
-    function btn_event(id, bt_id, speed) {
-      document.getElementById(id).ontouchstart =  function() {
-      var my_interval = setInterval(function() { btn_id = bt_id; send(player); }, speed);
-      this.ontouchend = function(){ clearInterval(my_interval); }
-      }
-    }
-
-    function save_event(id, bt_id) {
-      document.getElementById(id).onclick =  function() {
-        btn_id = bt_id; send(player);
-      }
-    }
-
-    function _port(id, prt) {
-      document.getElementById(id).onclick =  function() {
-        port = prt;
-        host = "ws://"+window.location.hostname.toString()+":"+port;
-        connect(player);
-      }
-    }
-
-    connect(player);
-
-    btn_event("left", 1, move_speed);
-    btn_event("top", 2, move_speed);
-    btn_event("down", 3, move_speed);
-    btn_event("right", 4, move_speed);
-    btn_event("fire", 5, fire_speed);
-    save_event("save", 6);
-    _port("multi", 3010);
-    _port("single", 3020);
-}
